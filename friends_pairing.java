d5f706c585efa1850d7f98adfae5355fcf003820
Saurabh Anand <jhasaurabhanand1997@gmail.com>
2020-09-25 13:34:57 +0530

    Friends Pairing Problem

---
+++ friends_pairing.java
@@ -0,0 +1,21 @@
+//Given n friends, each one can remain single or can be paired up with some other friend. Each friend can be paired only once. 
+//Find out the total number of ways in which friends can remain single or can be paired up.
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner scn=new Scanner(System.in);
+        int n=scn.nextInt();
+        int[] dp=new int[n+1];
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3;i<=n;i++){
+            dp[i]=dp[i-1]+(i-1)*dp[i-2];
+        }
+    System.out.println(dp[n]);
+    }
+
+}
